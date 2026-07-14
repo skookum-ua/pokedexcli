c38a34c2f9284c3417a3e909ec72b875ea7d1bd8
skookum-ua <skookum.ua@gmail.com>
2026-07-14 12:17:56 +0000

    repl

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 import (
 	"fmt"
- "strings"
+	"strings"
+	"os"
+	"bufio"
 )
 func main(){
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
+		userInput := scanner.Text()
+		userCleanInput := cleanInput(userInput)
+		if len(userCleanInput) != 0{
+			fmt.Printf("Your command was: %s\n", userCleanInput[0] )
+			
+		}
+	}
 }
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
